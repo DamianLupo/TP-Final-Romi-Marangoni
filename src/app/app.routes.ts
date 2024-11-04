9baa7665482ce83addd4538c7a6685892c32bf22
mancmateo <mancmateo@gmail.com>
2024-11-03 21:40:28 -0300

    agregado de apartado politicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { PoliticasComponent } from './components/politicas/politicas.component';
 
 export const routes: Routes = [
   { path: '', component: InicioSesionComponent },
   { path: 'login', component: InicioSesionComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  {path:'sobre-nosotros', component: AboutUsComponent}
+  {path:'sobre-nosotros', component: AboutUsComponent},
+  {path: 'terminos-y-condiciones', component: PoliticasComponent}
 ];
