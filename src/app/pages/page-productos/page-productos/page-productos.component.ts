73e1d5ad6770ce68631f38aa8be6bf57f7730afc
mancmateo <mancmateo@gmail.com>
2024-11-10 16:11:32 -0300

    se creo la pagina de productos y se muestran, ademas se linkeo con details-productos

--- src/app/pages/page-productos/page-productos/page-productos.component.ts
+++ src/app/pages/page-productos/page-productos/page-productos.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
 import { ListProductosComponent } from "../../../components/list-productos/list-productos.component";
+import { Producto } from "../../../interface/producto.interface";
+import { ProductoService } from "../../../service/producto.service";
+import { RouterLink } from "@angular/router";
+import { CommonModule } from "@angular/common";
+
 
 @Component({
   selector: 'app-page-productos',
   standalone: true,
-  imports: [ListProductosComponent],
+  imports: [CommonModule, RouterLink],
   templateUrl: './page-productos.component.html',
   styleUrl: './page-productos.component.css'
 })
 
 export class PageProductosComponent {
+
+  ngOnInit(): void{
+    this.listarProductos();
+  }
+  listaProductos : Producto[] = [];
+
+  productoService = inject(ProductoService);
+
+  listarProductos(){
+    this.productoService.getProductos().subscribe({
+      next: (productos : Producto[]) =>{
+        this.listaProductos = productos;
+      },
+      error: (e : Error) =>{
+        console.log(e.message);
+      }
+    })
+  }
 
 }
---
+++ src/app/productos-details/productos-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './productos-details.component.html',
+  styleUrl: './productos-details.component.css'
+})
+export class ProductosDetailsComponent {
+
+}
