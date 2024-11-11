9a27f85b184f2c5fb6d2baac648368a0d2fb4786
Luciano <luchogoma342@gmail.com>
2024-11-11 12:39:52 -0300

    Se actualizo la API de mercado pago y se consumio correctamente.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,16 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
 import { ListProductosComponent } from "./components/list-productos/list-productos.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InicioSesionComponent, FooterComponent, HeaderComponent, ListProductosComponent],
+  imports: [CommonModule, RouterOutlet, InicioSesionComponent, FooterComponent, HeaderComponent, ListProductosComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   router = inject(Router);
   title = 'tp-final';
--- src/app/components/productos-details/productos-details.component.ts
+++ src/app/components/productos-details/productos-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { ProductoService } from '../service/producto.service';
+import { ProductoService } from '../../service/producto.service';
+import { MercadoPagoService } from '../../service/mercado-pago.service';
 
 @Component({
   selector: 'app-productos-details',
@@ -22,5 +23,23 @@ export class ProductosDetailsComponent implements OnInit {
 
   obtenerProducto(id: string | null){
     this.productoService.getProducto(id).subscribe(producto => this.producto = producto);
+  }
+
+  constructor(private mercadoPagoService: MercadoPagoService) {}
+
+  createPaymentPreference() {
+    const title = this.producto.nombre;
+    const quantity = 1;
+    const unitPrice =  this.producto.precio;
+
+    this.mercadoPagoService.createPreference(title, quantity, unitPrice).subscribe(
+      response => {
+        console.log('ID de la preferencia:', response.id);
+        window.location.href = response.init_point; 
+      },
+      error => {
+        console.error('Error al crear la preferencia', error);
+      }
+    );
   }
 }
